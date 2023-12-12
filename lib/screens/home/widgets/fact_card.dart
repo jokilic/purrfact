dda2f7af045f899683e84a2eb0b3f5d6934f0eca
Josip Kilić <neksuses@gmail.com>
2023-12-12 16:00:27 +0100

    Longer facts are scrollable

--- lib/screens/home/widgets/fact_card.dart
+++ lib/screens/home/widgets/fact_card.dart
@@ -13,31 +13,31 @@ class FactCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Card(
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: AnimatedSize(
-              duration: const Duration(milliseconds: 200),
-              curve: Curves.easeIn,
-              child: ConstrainedBox(
-                constraints: const BoxConstraints(
-                  minHeight: 120,
-                  maxHeight: 280,
-                  minWidth: double.infinity,
-                ),
-                child: AnimatedSwitcher(
-                  duration: const Duration(milliseconds: 200),
-                  switchInCurve: Curves.easeIn,
-                  switchOutCurve: Curves.easeIn,
-                  child: isLoading
-                      ? FactLoading()
-                      : Text(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: AnimatedSize(
+            duration: const Duration(milliseconds: 200),
+            curve: Curves.easeIn,
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(
+                minHeight: 120,
+                maxHeight: 280,
+                minWidth: double.infinity,
+              ),
+              child: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 200),
+                switchInCurve: Curves.easeIn,
+                switchOutCurve: Curves.easeIn,
+                child: isLoading
+                    ? FactLoading()
+                    : SingleChildScrollView(
+                        physics: const BouncingScrollPhysics(),
+                        child: Text(
                           text,
                           style: Theme.of(context).textTheme.bodyLarge,
                           textAlign: TextAlign.center,
                         ),
-                ),
+                      ),
               ),
             ),
           ),
